461ee0d98324f7fd445beac80593157c382b4002
mu-777 <water.tree.rainbow@gmail.com>
2021-01-31 17:25:47 +0900

    Add test codes for blog

---
+++ Assets/ForBlog/TestCubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using toio;
+
+public class TestCubeController : MonoBehaviour
+{
+    Cube _cube;
+
+    async void Start()
+    {
+        var peripheral = await new NearestScanner().Scan();
+        _cube = await new CubeConnecter().Connect(peripheral);
+    }
+
+    void Update()
+    {
+        Debug.Log(_cube.GetPosInMatUV());
+    }
+}
